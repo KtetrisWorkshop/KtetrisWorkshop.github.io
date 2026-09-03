a63c9e080faf4ba11fcf15833e6633110a6e5670
Neptrue-Lin <29837162+Neptrue-Lin@users.noreply.github.com>
2026-09-03 16:01:40 +0800

    build(project): Initialize project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'Ktetris',
+
+  theme: defaultTheme({
+    navbar: [
+	{
+	  text: "Workshop",
+	  link: "https://github.com/KtetrisWorkshop",
+	},
+	{
+	  text: "Server",
+	  link: "https://github.com/KtetrisWorkshop/KtetrisServer",
+	},
+	{
+	  text: "Client",
+	  link: "https://github.com/KtetrisWorkshop/KtetrisClient",
+	},
+    ],
+    sidebar: [
+		"README.md",
+    ]
+  }),
+
+  bundler: viteBundler(),
+})
